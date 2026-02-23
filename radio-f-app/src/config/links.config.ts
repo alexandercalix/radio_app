7f8faf509949a52032c091e82037a1efe40b271f
oscarcalix <alexander.calix1995@hotmail.com>
2026-02-23 05:24:39 -0600

    Deletion of React Native APp

--- radio-f-app/src/config/links.config.ts
+++
@@ -1,7 +0,0 @@
-export const LINKS = {
-  website: "https://www.fronterahn.com",
-  facebook: "https://www.facebook.com/Frontera95.1FM",
-  instagram: "https://www.instagram.com/frontera_95.1fm",
-  tiktok: "https://www.tiktok.com/@frontera95.1fm",
-   whatsapp: "https://wa.me/50432352141",
-};
